285e4366f9a2d23412744cf80c261e03116f9962
zk0208 <2992673907@qq.com>
2022-12-14 18:09:49 +0800

    init commit

--- build/third_party/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhuo1/lsmf/leveldb-1.23/third_party/googletest/googletest
-# Build directory: /home/zhuo1/lsmf/leveldb-1.23/build/third_party/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
